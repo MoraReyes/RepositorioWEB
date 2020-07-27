7f638d1e0bc0842ef5f7163b33375492f45f3a60
VICTOR JOAQUIN LOPEZ CASTILLO <48496874+victorLopz@users.noreply.github.com>
2020-07-26 18:19:29 -0600

    Prueba

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<?php
+
+	$destino= "juanmorareyes99@gmail.com";
+	$nombre = $_POST["name"];
+	$correo = $_POST["email"];
+	$sujeto = $_POST["subject"];
+	$mensaje = $_POST["message"];
+
+	$contenido = "Nombre".$nombre."\nEmail".$correo."\nSujeto".$sujeto."Mensaje".$mensaje;
+
+	echo $contenido   
+
+	mail($destino,"Contacto",$contenido);
+	header("Location:index.html");
+
+?>
